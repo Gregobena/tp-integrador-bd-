2c4c51d9ec830c7f31e6c925461a0bf3e9f86373
Gregorio Benavidez <geonobena@gmail.com>
2025-10-20 13:15:11 -0300

    parte de redis casi terminada

--- bd-contenedor-main/bd-contenedor-main/notebooks/funciones_mongo.py
+++ bd-contenedor-main/bd-contenedor-main/notebooks/funciones_mongo.py
@@ -7,6 +7,19 @@ MONGO_PASS = os.getenv("MONGO_INITDB_ROOT_PASSWORD", "admin123")
 
 client = MongoClient(f"mongodb://{MONGO_USER}:{MONGO_PASS}@mongo:27017/")
 db = client["viajes"]
+def iniciar_mongo(): 
+    from pymongo import MongoClient
+    import os 
+    import csv
+
+    MONGO_USER = os.getenv("MONGO_INITDB_ROOT_USERNAME", "admin")
+    MONGO_PASS = os.getenv("MONGO_INITDB_ROOT_PASSWORD", "admin123")
+
+    client = MongoClient(f"mongodb://{MONGO_USER}:{MONGO_PASS}@mongo:27017/")
+    db = client["viajes"]
+    return db, True 
+
+db, estado = iniciar_mongo()
 
 def insertar_documento(coleccion, documento):
     db[coleccion].insert_one(documento)
@@ -14,6 +27,5 @@ def insertar_documento(coleccion, documento):
 def insertar_varios_documentos(coleccion, lista_documentos):
     for elem in lista_documentos:
         db[coleccion].insert_one(elem)
-
-
-
+def find_(coleccion,argumento):
+    return db.coleccion.find(argumento)
